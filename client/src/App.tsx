510d2b097b4a45af22cbf202426a5145b75be60e
naitik-popli <natik9588@gmail.com>
2025-07-17 23:22:49 +0530

    T3 test

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,7 +6,6 @@ import { TooltipProvider } from "./components/ui/tooltip";
 import MoodChat from "./pages/mood-chat";
 import NotFound from "./pages/not-found";
 import LocalStreamTest from "./pages/local-stream-test";
-import { setupWebSocket } from "./websocket";
 
 function Router() {
   return (
