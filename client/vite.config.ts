ed9652d10c60bf2507f65a519689ecc749a136bb
naitik-popli <natik9588@gmail.com>
2025-07-19 13:34:56 +0530

    T2.39 test

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
   root: 'client',
+  plugins: [react()],
   server: {
     port: 3000,
     host: '0.0.0.0', // Allow access from other devices on the network
@@ -12,9 +12,6 @@ export default defineConfig({
     alias: {
       '@': '/client/src',
     },
-  },
-  optimizeDeps: {
-    include: ['simple-peer'],
   },
   build: {
     sourcemap: true,
